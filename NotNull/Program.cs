b04e11573a7fd19a88c68552387e7c77851fc36c
FLOAT_WORLD <253150972@qq.com>
2023-09-18 17:02:48 +0800

    First commit

---
+++ NotNull/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace NotNull
+{
+    public class NotNullable
+    {
+        public NotNullable()
+        {
+            Type type = GetType();
+            foreach (FieldInfo fieldInfo in type.GetFields(BindingFlags.Instance|BindingFlags.NonPublic|BindingFlags.Public))
+            {
+                foreach(NotNull o in fieldInfo.GetCustomAttributes(typeof(NotNull),true))
+                {
+                    if (fieldInfo.GetValue(this) == null)
+                    {
+                        throw new Exception("This Property can't be null");
+                    }
+                }
+            }
+        }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
+    sealed class NotNull : Attribute
+    {
+    }
+}
